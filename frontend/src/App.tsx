fc0baf6bea2b5b50f861f0527663aad19ecf869d
matheushajer <matheushajer@gmail.com>
2022-07-12 10:22:53 -0300

    Criação do componente cartãoVendas  e ajustes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
-import BotaoNotificacao from "./asstes/componentes/botaoNotificacao"
+import BotaoNotificacao from "./asstes/componentes/BotaoNotificacao"
 import Header from "./asstes/componentes/Header"
+import CartaoVendas from "./asstes/componentes/CartaoVendas"
 
 function App() {
   return (
     <>
       <Header />
-      <BotaoNotificacao />
-      
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <CartaoVendas />
+          </div>
+        </section>
+      </main>
+
     </>
   )
 }
