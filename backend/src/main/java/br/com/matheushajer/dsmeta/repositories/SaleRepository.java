781a57cffd63939efc5a83f0b2a730f0a8862ef4
matheushajer <matheushajer@gmail.com>
2022-07-13 12:52:24 -0300

    Get findAll

---
+++ backend/src/main/java/br/com/matheushajer/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,11 @@
+package br.com.matheushajer.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.matheushajer.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+	
+	
+}
---
+++ backend/src/main/java/br/com/matheushajer/dsmeta/services/SaleService.java
@@ -0,0 +1,21 @@
+package br.com.matheushajer.dsmeta.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.matheushajer.dsmeta.entities.Sale;
+import br.com.matheushajer.dsmeta.repositories.SaleRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository;
+	
+	public List<Sale> findSales() {
+		return repository.findAll();
+	}
+	
+}
