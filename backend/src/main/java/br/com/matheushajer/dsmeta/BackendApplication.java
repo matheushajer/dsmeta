6fe3866952593402984d0d26d65fcc3d7d7e9c49
matheushajer <matheushajer@gmail.com>
2022-07-13 09:52:08 -0300

    Configuração de segurança e ajustes em pacotes

--- backend/src/main/java/br/com/matheushajer/dsmeta/BackendApplication.java
+++ backend/src/main/java/br/com/matheushajer/dsmeta/BackendApplication.java
@@ -1,4 +1,4 @@
-package br.com.dsmeta.backend;
+package br.com.matheushajer.dsmeta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/br/com/matheushajer/dsmeta/BackendApplicationTests.java
+++ backend/src/test/java/br/com/matheushajer/dsmeta/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.dsmeta.backend;
+package br.com.matheushajer.dsmeta;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
