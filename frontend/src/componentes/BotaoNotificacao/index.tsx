0fcb6c8fe8f954ac79468255d11322491b5af796
matheushajer <matheushajer@gmail.com>
2022-07-15 10:58:30 -0300

    Ajustes gerais das pastas

--- frontend/src/componentes/BotaoNotificacao/index.tsx
+++ frontend/src/componentes/BotaoNotificacao/index.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
-import { BASE_URL } from '../../../utils/request';
+import { BASE_URL } from '../../utils/request';
 import icone from '../../img/icone-notificacao.svg'
 import './styles.css'
 
